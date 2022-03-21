8d8d018297319129044722e6603565b2bbd78ef3
robert <snellro24@spaldinghs.org>
2022-03-21 16:58:22 -0400

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,1 +1,14 @@
 #quiz
+from questions import questions
+
+score = 0
+attempts = 0
+
+#Check answers to see if they are correct or not
+def checkAnswer(question, answer, score, attempts):
+    if questions[question]['answer'].lower == answer.lower:
+        print("Correct answer! Your score is ",(score + 1))
+        return True
+    else:
+        print("Incorrect answer! You have", attempts,"remaining.")
+        return False
