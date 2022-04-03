ba94303b498d252df6390ec66e0fcadfec483a13
Robert Snell <snellrobert06@gmail.com>
2022-04-02 22:23:03 -0400

    fix: a few changes

--- questions.py
+++ questions.py
@@ -5,15 +5,10 @@ quiz = {
         "answer" : "Michelle"
     },
     2 : {
-        "question" : "How many degrees are in a circle?",
-        "answer" : "360 degrees",
-        "answer" : "360"
-    },
-    3 : {
         "question" : "How many teeth are in the adult mouth?",
         "answer" : "32"
     },
-    4 : {
+    3 : {
         "question" : "How many days are in a fortnight?",
         "answer" : "14"
     }
--- quiz.py
+++ quiz.py
@@ -3,13 +3,15 @@ from questions import quiz
 
 score = 0
 
+print("Welcome to my game! Answer the trivia question with the correct answer to earn a point. Try repeating the game as many times you want to score the maximum amount of points. Good luck!")
+
 #Check answers to see if they are correct or not
 def checkAnswer(question, answer, score, attempts):
     if quiz[question]['answer'].lower == answer.lower:
         print("Correct answer! Your score is ",(score + 1))
         return True
     else:
-        print("Incorrect answer! You have", attempts,"remaining.")
+        print("Incorrect answer! You have", attempts - 1,"attempts remaining.")
         return False 
 
 while True:
@@ -17,6 +19,7 @@ while True:
         attempts = 3
         while attempts > 0:
             print(quiz[question]['question'])
+            print(quiz[question]['answer'])
             answer = input("Enter your answer, to skip the question type 'skip': ")
             if answer.lower == "skip":
                 break
